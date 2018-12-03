3fb2ec23f7d8bc10c521606c4799d36c8aa9c3a9
William Teston <wjteston@gmail.com>
2018-12-03 16:45:02 -0500

    uploading files

---
+++ js/main.js
@@ -0,0 +1,4 @@
+require_once 'meekrodb.2.3.class.php';
+DB::$user = 'wteston';
+DB::$password = '25791484';
+DB::$dbName = 'aws377';
---
+++ php/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            //require_once 'meekrodb.2.3.class.php';
+            //DB::$user = 'wteston';
+            //DB::$password = '25791484';
+            //DB::$dbName = 'umdteston';
+
+            //$results = DB::query("SELECT * FROM schools");
+            //foreach ($results as $row) {
+            //    echo "School: " . $row['school'] . "\n";
+            //}
+        
+        
+        echo $_GET["class"];
+        echo $_GET["subject"];
+        echo $_GET["semester"];
+        ?>
+    </body>
+</html>
